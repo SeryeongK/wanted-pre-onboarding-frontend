64fb72084235d2bfe732af268cbdf44f462e79bb
SeryeongK <99seryoung@naver.com>
2023-08-06 23:15:12 +0900

    style: 회원가입 페이지 수정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signUp_api } from "../apis/SignUpApi";
+import { CiWarning } from "react-icons/ci";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
   const navigate = useNavigate();
 
@@ -21,52 +23,82 @@ export default function SignUp() {
       navigate("/signin");
     } else if (statusCode === 400) {
       console.log(statusCode, message);
+      setErrMsg(message);
     } else {
       console.log(err, message);
     }
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1>회원가입</h1>
-      <div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-1/2 h-2/3 flex flex-col justify-center items-center border-2 rounded-md">
+        <p className="text-xl py-[5rem]">회원가입</p>
         <div>
-          <p>이메일</p>
-          <input
-            type="text"
-            data-testid="email-input"
-            name="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
+          <div className="flex flex-row">
+            <p className="w-[5rem]">이메일</p>
+            <div className="h-[4rem]">
+              <input
+                type="text"
+                data-testid="email-input"
+                name="email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                className="w-56 border-b-2"
+              />
+              {!email.includes("@") && (
+                <p className="text-sm py-1 text-rose-600">
+                  형식이 잘못되었습니다.(@ 미포함)
+                </p>
+              )}
+            </div>
+          </div>
+          <div className="flex flex-row">
+            <p className="w-[5rem]">비밀번호</p>
+            <div className="h-[4rem]">
+              <input
+                type="text"
+                data-testid="password-input"
+                name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="w-56 border-b-2"
+              />
+              {password.length < 8 && (
+                <p className="text-sm py-1 text-rose-600">
+                  비밀번호는 8자 이상이어야 합니다.
+                </p>
+              )}
+            </div>
+          </div>
         </div>
-        {!email.includes("@") && (
-          <p>이메일 형식이 잘못되었습니다. * @가 포함되어야 합니다.</p>
+        {errMsg.length > 1 && (
+          <div className="px-3 py-2 rounded bg-rose-400 flex flex-row">
+            <CiWarning className="text-white" />
+            <p className="text-xs text-white px-1">{errMsg}</p>
+            <CiWarning className="text-white" />
+          </div>
         )}
-        <div>
-          <p>비밀번호</p>
-          <input
-            type="text"
-            data-testid="password-input"
-            name="password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
+        <div className="py-[1.5rem]">
+          <button
+            data-testid="signup-button"
+            disabled={!email.includes("@") || password.length < 8}
+            className="px-[7.7rem] py-2 rounded bg-blue-500 text-white disabled:bg-opacity-50 disabled:cursor-not-allowed"
+            onClick={handleSignUp}
+          >
+            회원가입하기
+          </button>
         </div>
-        {password.length < 8 && <p>비밀번호는 8자 이상이어야 합니다.</p>}
+        <div className="flex flex-row ">
+          <p className="px-2 text-xs text-gray-500">이미 회원이신가요?</p>
+          <p onClick={() => navigate("/signin")} className="px-2 text-xs">
+            로그인하기
+          </p>
+        </div>
       </div>
-      <button
-        data-testid="signup-button"
-        disabled={!email.includes("@") || password.length < 8}
-        className="px-4 py-2 rounded bg-blue-500 text-white disabled:bg-opacity-50 disabled:cursor-not-allowed"
-        onClick={handleSignUp}
-      >
-        회원가입하기
-      </button>
     </div>
   );
 }
