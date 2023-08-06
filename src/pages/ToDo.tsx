751c93f3f9fff8f014f54ecb7ec26af682c0cc9c
SeryeongK <99seryoung@naver.com>
2023-08-06 23:15:42 +0900

    style: 투두 리스트 페이지 수정

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -87,97 +87,115 @@ export default function Todo() {
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div>
-        <h1>TODO</h1>
-        <button
-          onClick={() => {
-            localStorage.setItem("token", "");
-            navigate("/signin");
-          }}
-        >
-          로그아웃
-        </button>
-      </div>
-      <div>
-        <input
-          type="text"
-          data-testid="new-todo-input"
-          name="new_todo"
-          value={newTodo}
-          onChange={(e) => {
-            setNewTodo(e.target.value);
-          }}
-        />
-        <button data-testid="new-todo-add-button" onClick={handleCreate}>
-          추가
-        </button>
-      </div>
-      <div>
-        {todos.map((t) => {
-          return (
-            <li key={t.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={t.isCompleted}
-                  onChange={() => handleUpdate(t.id, t.todo, !t.isCompleted)}
-                />
-                {!t.isEditing ? (
-                  <span>{t.todo}</span>
-                ) : (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-1/2 h-2/3 flex flex-col justify-center items-center border-2 rounded-md">
+        <div className="flex flex-col w-full">
+          <p className="text-3xl text-center">TODO</p>
+          <div className="flex w-full justify-end pr-4">
+            <button
+              onClick={() => {
+                localStorage.setItem("token", "");
+                navigate("/signin");
+              }}
+              className="text-sm text-gray-500"
+            >
+              로그아웃
+            </button>
+          </div>
+        </div>
+        <div className="w-full px-10 flex flex-row justify-between py-[2rem]">
+          <input
+            type="text"
+            data-testid="new-todo-input"
+            name="new_todo"
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+            className="w-full border-b-2"
+          />
+          <button
+            data-testid="new-todo-add-button"
+            onClick={handleCreate}
+            className="w-16 px-2 py-1 bg-blue-500 text-white rounded mx-2"
+          >
+            추가
+          </button>
+        </div>
+        <div className="w-full px-10">
+          {todos.map((t) => {
+            return (
+              <li
+                key={t.id}
+                className="list-none w-full px-2 py-2 my-1 flex justify-between"
+              >
+                <label className="w-full bg-slate-200 w-full px-2 py-2">
                   <input
-                    type="text"
-                    data-testid="modify-input"
-                    name="edited_todo"
-                    value={editedTodo}
-                    onChange={(e) => {
-                      setEditedTodo(e.target.value);
-                    }}
+                    type="checkbox"
+                    checked={t.isCompleted}
+                    onChange={() => handleUpdate(t.id, t.todo, !t.isCompleted)}
+                    className="mx-2"
                   />
+                  {!t.isEditing ? (
+                    <span>{t.todo}</span>
+                  ) : (
+                    <input
+                      type="text"
+                      data-testid="modify-input"
+                      name="edited_todo"
+                      value={editedTodo}
+                      onChange={(e) => {
+                        setEditedTodo(e.target.value);
+                      }}
+                    />
+                  )}
+                </label>
+                {!t.isEditing ? (
+                  <>
+                    <button
+                      data-testid="modify-button"
+                      onClick={() => {
+                        handleEdit(t.id);
+                        setEditedTodo(t.todo);
+                      }}
+                      className="w-16 px-2 bg-blue-500 text-white rounded mx-2"
+                    >
+                      수정
+                    </button>
+                    <button
+                      data-testid="delete-button"
+                      onClick={() => {
+                        handleDeleteTodo(t.id);
+                      }}
+                      className="w-16 px-2 bg-rose-500 text-white rounded"
+                    >
+                      삭제
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <button
+                      data-testid="submit-button"
+                      onClick={() =>
+                        handleUpdate(t.id, editedTodo, t.isCompleted)
+                      }
+                      className="w-16 px-2 bg-blue-500 text-white rounded mx-2"
+                    >
+                      제출
+                    </button>
+                    <button
+                      data-testid="cancel-button"
+                      onClick={() => handleEdit(t.id)}
+                      className="w-16 px-2 bg-rose-500 text-white rounded"
+                    >
+                      취소
+                    </button>
+                  </>
                 )}
-              </label>
-              {!t.isEditing ? (
-                <>
-                  <button
-                    data-testid="modify-button"
-                    onClick={() => {
-                      handleEdit(t.id);
-                      setEditedTodo(t.todo);
-                    }}
-                  >
-                    수정
-                  </button>
-                  <button
-                    data-testid="delete-button"
-                    onClick={() => {
-                      handleDeleteTodo(t.id);
-                    }}
-                  >
-                    삭제
-                  </button>
-                </>
-              ) : (
-                <>
-                  <button
-                    data-testid="submit-button"
-                    onClick={() =>
-                      handleUpdate(t.id, editedTodo, t.isCompleted)
-                    }
-                  >
-                    제출
-                  </button>
-                  <button
-                    data-testid="cancel-button"
-                    onClick={() => handleEdit(t.id)}
-                  >
-                    취소
-                  </button>
-                </>
-              )}
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
